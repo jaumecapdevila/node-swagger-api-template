12b288278fe68692f31c46d22d725441b9e1a1ff
jaumecapdevila <contact@jaumecapdevila.net>
2017-09-21 11:48:47 +0200

    Added a uuid field to the base model

--- src/context/domain/model/base.js
+++ src/context/domain/model/base.js
@@ -3,6 +3,16 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const BaseSchema = new Schema({
+  uuid: {
+    type: String,
+    required: true,
+    validate: {
+      validator(value) {
+        return /\d{8}-\d{4}-\d{4}-\d{4}-\d{12}/.test(value);
+      },
+      message: '{value} is not a valid uuid!',
+    },
+  },
   propertyA: {
     type: String,
     required: true,
