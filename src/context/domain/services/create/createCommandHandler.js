cf2fac3e5dd3cd9de872f8032333358f01eddf4e
jaumecapdevila <contact@jaumecapdevila.net>
2017-09-21 11:57:26 +0200

    Updated the create service with the new model field

--- src/context/domain/services/create/createCommandHandler.js
+++ src/context/domain/services/create/createCommandHandler.js
@@ -1,12 +1,15 @@
 const BaseModel = require('../../model/base');
+const uuidv4 = require('uuid/v4');
 
 function CreateCommandHandler() {}
 
 CreateCommandHandler.execute = function execute(command) {
   const { propertyA, propertyB } = command;
+  const uuid = uuidv4();
   return new Promise((resolve, reject) => {
     try {
       const model = new BaseModel({
+        uuid,
         propertyA,
         propertyB,
       });
