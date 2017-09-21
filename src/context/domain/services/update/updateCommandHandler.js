6f70921cc73c8951e3dbb107b1058828d5a83e11
jaumecapdevila <contact@jaumecapdevila.net>
2017-09-21 16:04:03 +0200

    Added validation to the use case to update an existent model

--- src/context/domain/services/update/updateCommandHandler.js
+++ src/context/domain/services/update/updateCommandHandler.js
@@ -1,23 +1,45 @@
 const BaseModel = require('../../model/base');
-const uuidv4 = require('uuid/v4');
 
 function UpdateCommandHandler() {}
 
 UpdateCommandHandler.execute = function execute(command) {
   const INVALID_REQUEST_HTTP_CODE = 400;
+  const RESOURCE_NOT_FOUND_HTTP_CODE = 404;
   const INTERNAL_SERVER_ERROR_HTTP_CODE = 500;
   const { uuid, propertyA, propertyB } = command;
   return new Promise((resolve, reject) => {
     try {
-      BaseModel.update({ uuid }, { uuid, propertyA, propertyB }, {}, (error) => {
-        if (error) {
+      BaseModel.findOne({ uuid }, 'uuid propertyA propertyB', (findErrors, model) => {
+        if (findErrors) {
           reject({
-            statusCode: INVALID_REQUEST_HTTP_CODE,
-            errors: error.errors,
+            statusCode: INTERNAL_SERVER_ERROR_HTTP_CODE,
+            errors: {
+              message: 'An unexpected error has ocurred, please try it again later.',
+            },
           });
           return;
         }
-        resolve({});
+        if (!model) {
+          reject({
+            statusCode: RESOURCE_NOT_FOUND_HTTP_CODE,
+            errors: {
+              message: `The model identified with ${uuid} was not found.`,
+            },
+          });
+          return;
+        }
+        model.propertyA = propertyA;
+        model.propertyB = propertyB;
+        model.save((saveErrors) => {
+          if (!saveErrors) {
+            resolve({});
+            return;
+          }
+          reject({
+            statusCode: INVALID_REQUEST_HTTP_CODE,
+            errors: saveErrors.errors,
+          });
+        });
       });
     } catch (error) {
       reject({
